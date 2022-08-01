8593bf20376b360b05a99ddbe550a69e62f565cc
GokuSuperSaiyan72 <KunalRajput7656@gmail.com>
2022-08-01 18:26:56 +0530

    rvpreact intialised and added firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAG7aYqGunCEEqMePnR7YN_uMsn8yvwtcM",
+  authDomain: "reverr-25fb3.firebaseapp.com",
+  databaseURL: "https://reverr-25fb3-default-rtdb.firebaseio.com",
+  projectId: "reverr-25fb3",
+  storageBucket: "reverr-25fb3.appspot.com",
+  messagingSenderId: "710745964607",
+  appId: "1:710745964607:web:9c0b08192f30bb97bab88a",
+  measurementId: "G-7S7P5C52RG",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
