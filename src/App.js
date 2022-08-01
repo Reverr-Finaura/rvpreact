84b9f3cfeef0bab951906abc749593475c91df41
Ayush Sharma <ayush2162020@gmail.com>
2022-08-01 19:54:33 +0530

    testing local env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <h1>Hello Reverr</h1>
-    </>
+      <h2>Testing local setup</h2>
+    </React.Fragment>
   );
 }
 
