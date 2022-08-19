bbb0e1256f58815abd2cd85246179bc82a6fee2d
Ayush Sharma <ayush2162020@gmail.com>
2022-08-20 04:32:34 +0530

    obsolete media query removed

--- src/components/loggedInNavbar/LoggedInNavbar.js
+++ src/components/loggedInNavbar/LoggedInNavbar.js
@@ -3,23 +3,23 @@ import logo from "../../assets/vectors/Reverr Black 2.png";
 import { useNavigate } from "react-router-dom";
 import { Bell, QuestionCircle, Person } from "react-bootstrap-icons";
 
-const LogedInNavbar = () => {
+const LoggedInNavbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="loged-in__navbar">
-      <div className="loged-in__reverr">
-        <img src={logo} alt="reverr-logo" className="loged-in__logo" />
-        <div className="loged-in__title">REVERR VENTURE PARTNERS</div>
+    <div className="logged-in__navbar">
+      <div className="logged-in__reverr">
+        <img src={logo} alt="reverr-logo" className="logged-in__logo" />
+        <div className="logged-in__title">REVERR VENTURE PARTNERS</div>
       </div>
-      <div className="loged-in__links">
-        <span className="loged-in__link">
+      <div className="logged-in__links">
+        <span className="logged-in__link">
           <Bell />
         </span>
-        <span className="loged-in__link">
+        <span className="logged-in__link">
           <QuestionCircle />
         </span>
-        <span className="loged-in__link" onClick={() => navigate("/profile")}>
+        <span className="logged-in__link" onClick={() => navigate("/profile")}>
           <Person />
         </span>
       </div>
@@ -27,4 +27,4 @@ const LogedInNavbar = () => {
   );
 };
 
-export default LogedInNavbar;
+export default LoggedInNavbar;
